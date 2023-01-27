70cc8a295543c07ec9d70d06dc1431bd62452a8e
Nick Fortescue <nfortescue@google.com>
2023-01-27 16:31:56 +0000

    Set up nhstravel using setup.py. Add it to the streamlit dependencies. Add a sample page demonstrating the use of common library code in streamlit.

---
+++ nhstravel/info.py
@@ -0,0 +1,18 @@
+"""This module exists just to allow an easy check that the nhstravel module dependencies work.
+
+It has two basic ways to test it:
+
+from nhstravel.info import description
+
+or
+
+from nhstravel.info import NHS_TRAVEL_DESCRIPTION
+
+"""
+
+NHS_TRAVEL_DESCRIPTION = "A library to help calculate geographical information for the NHS."
+
+
+def description():
+    """Get a string that describes what the nhstravle library does"""
+    return NHS_TRAVEL_DESCRIPTION
